b7043e61f6abfbb17b129951738dc9a41429aa32
Raphaël Monnard <raphael@monnard.email>
2021-06-06 21:59:29 +0200

    NEW bootstrap test page

--- interlab/urls.py
+++ interlab/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
+from . import views
 
 admin.autodiscover()
 
@@ -15,6 +16,7 @@ urlpatterns += [
     path("admin/", admin.site.urls),
     path("accounts/", include('django.contrib.auth.urls')),
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path('bootstrap/', views.bootstrap, name='bootstrap'),
     path("", include("cms.urls")),
 ]
 
