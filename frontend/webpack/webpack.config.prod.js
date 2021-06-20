681385653fefbb47722939ccde29caafa4b79a59
Raphaël Monnard <raphael@monnard.email>
2021-06-20 08:57:31 +0200

    Production build

--- frontend/webpack/webpack.config.prod.js
+++ frontend/webpack/webpack.config.prod.js
@@ -6,10 +6,9 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  stats: 'errors-only',
   bail: true,
   output: {
-    filename: 'js/[name].[chunkhash:8].js',
+    filename: 'js/[name].js',
     chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
   },
   plugins: [
@@ -17,7 +16,7 @@ module.exports = merge(common, {
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
     new MiniCssExtractPlugin({
-      filename: 'bundle.css',
+      filename: 'css/app.css',
     }),
   ],
   module: {
