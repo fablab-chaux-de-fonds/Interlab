dccce49f3187851f9421f06958cbec28300e1ae8
Raphaël Monnard <raphael@monnard.email>
2021-07-04 17:12:52 +0200

    Add img as static file / bootstrap logo

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -1,9 +1,9 @@
 require('bootstrap-icons/font/bootstrap-icons.css');
 import "../styles/index.scss";
-
-import $ from "jquery/dist/jquery.slim";
 import "bootstrap/dist/js/bootstrap.bundle";
 
-$(document).ready(function () {
-  window.console.log("dom ready");
-});
+function importAll(r) {
+    return r.keys().map(r);
+  }
+  
+  const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
