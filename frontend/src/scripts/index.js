975979ca4425e5145aded2f4390ad7e497367f9c
Raphaël Monnard <raphael@monnard.email>
2021-07-07 20:17:41 +0200

    Beautify code

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -6,4 +6,4 @@ function importAll(r) {
     return r.keys().map(r);
   }
   
-  const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
+const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));
