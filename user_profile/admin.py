e747a194fc6769c71097d3ce4ce75c8be986cfb6
Raphaël Monnard <raphael@monnard.email>
2021-07-08 20:01:09 +0200

    add admin controles

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'subscription')
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('category', 'expiration')
+
+@admin.register(SubscriptionCategory)
+class SubscriptionCategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'access_number')
--- user_profile/models.py
+++ user_profile/models.py
@@ -6,13 +6,18 @@ class SubscriptionCategory(models.Model):
     price = models.FloatField()
     access_number = models.PositiveSmallIntegerField()
 
+    def __str__(self):
+        return f"{self.title}"
+
 class Subscription(models.Model):
     expiration = models.DateField()
     category = models.ForeignKey(SubscriptionCategory, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.category.title}, {self.expiration}"
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     newsletter = models.BooleanField()
-    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
-
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE, blank=True, null=True)
 
