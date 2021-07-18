5a84ed3c3e45d780f24218f43dff3108497ab704
Raphaël Monnard <raphael@monnard.email>
2021-07-18 22:04:19 +0200

    add start date to subscription and adjuste verbose

--- user_profile/models.py
+++ user_profile/models.py
@@ -8,16 +8,23 @@ class SubscriptionCategory(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+    
+    class Meta:
+        verbose_name_plural = "Subscription Categories"
 
 class Subscription(models.Model):
-    expiration = models.DateField()
+    start = models.DateField()
+    end = models.DateField()
     category = models.ForeignKey(SubscriptionCategory, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.category.title}, {self.expiration}"
+        return f"{self.category.title}, {self.start}, {self.end}"
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     newsletter = models.BooleanField()
     subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}, ({self.user.username})"
 
