a69916229586bb18eae00aaf8b3788199bddb498
Raphaël Monnard <raphael@monnard.email>
2021-07-18 22:05:54 +0200

    Change col display in admin

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -6,12 +6,12 @@ from .models import *
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'subscription')
+    list_display = ['user', 'newsletter', 'subscription']
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('category', 'expiration')
+    list_display = ['category', 'start', 'end']
 
 @admin.register(SubscriptionCategory)
 class SubscriptionCategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'access_number')
+    list_display = ['title', 'price', 'access_number']
