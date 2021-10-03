f8c28059dfc161f691744683e6c6856de0654c3a
Raphaël Monnard <raphael@monnard.email>
2021-10-03 20:51:54 +0200

    FIX message when delete category. Cascade Subscription and category

--- user_profile/models.py
+++ user_profile/models.py
@@ -7,26 +7,27 @@ class SubscriptionCategory(models.Model):
     access_number = models.PositiveSmallIntegerField()
     duration = models.PositiveSmallIntegerField(blank=True, null=True) # in days
     star_flag = models.BooleanField(blank=True, null=True)
-    sort = models.PositiveSmallIntegerField(blank=True, null=True) 
+    sort = models.PositiveSmallIntegerField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.title}"
-    
+
     class Meta:
         verbose_name_plural = "Subscription Categories"
 
 class Subscription(models.Model):
     start = models.DateField()
     end = models.DateField()
-    category = models.ForeignKey(SubscriptionCategory, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(SubscriptionCategory, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
+        if self.category==None:
+            return f"No category, {self.start}, {self.end}"
         return f"{self.category.title}, {self.start}, {self.end}"
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     active = models.BooleanField(blank=True, null=True)
-
     subscription = models.ForeignKey(Subscription, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
