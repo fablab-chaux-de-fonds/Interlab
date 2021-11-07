2f0a3f34d7fef7919e2177113d42b68c764640d1
Raphaël Monnard <raphael@monnard.email>
2021-11-07 18:26:00 +0100

    NEW profile view

--- accounts/views.py
+++ accounts/views.py
@@ -2,13 +2,21 @@ from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.decorators import login_required
 
-from .models import Accounts
+from .models import Profile, SubscriptionCategory
 
 @login_required
 def AccountsView(request):
+    user = request.user
+    subscription = Profile.objects.get(user_id=user.id).subscription
+
     template = loader.get_template('accounts/profile.html')
     context = {
-        'page_title': "My accounts",
-        'user': request.user
+        'page_title': "My account",
+        'user': user,
+        'subscription': subscription,    
     }
+
+    if subscription:
+       context['subscription_category']=SubscriptionCategory.objects.get(pk=subscription.category_id)
+
     return HttpResponse(template.render(context, request))
