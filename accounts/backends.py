5e907c1d75ca7a7504c2c4e13c22490f9891e370
Raphaël Monnard <raphael@monnard.email>
2021-12-05 20:50:07 +0100

    removes the username from the create method

--- accounts/backends.py
+++ accounts/backends.py
@@ -8,8 +8,9 @@ from django.shortcuts import render
 from django.utils.translation import gettext as _
 
 from .models import Profile
-
+#from .forms import RegistrationForm
 from organizations.backends.defaults import InvitationBackend
+
 
 class CustomInvitations(InvitationBackend):
     """
@@ -19,39 +20,13 @@ class CustomInvitations(InvitationBackend):
     def get_success_url(self):
         return reverse("profile")
 
-    def activate_view(self, request, user_id, token):
-        """
-        View function that activates the given User by setting `is_active` to
-        true if the provided information is verified.
-        """
-        try:
-            user = self.user_model.objects.get(id=user_id, is_active=False)
-        except self.user_model.DoesNotExist:
-            raise Http404(_("Your URL may have expired."))
-
-        if not PasswordResetTokenGenerator().check_token(user, token):
-            raise Http404(_("Your URL may have expired."))
-        
-        form = self.get_form(
-             data=request.POST or None, files=request.FILES or None, instance=user
-        )
-        
-        if form.is_valid():
-            form.instance.is_active = True
-            user = form.save()
-            user.set_password(form.cleaned_data["password1"])
-            user.save()
-            self.activate_organizations(user)
-            user = authenticate(
-                username=form.cleaned_data["username"],
-                password=form.cleaned_data["password1"],
-            )
-
-            # link profile / account / organization / subscription
-            subscription = user.organizations_organization.first().owner.organization_user.user.profile.subscription
-            profile = Profile(user=user, subscription=subscription)
-            profile.save()
-
-            login(request, user)
-            return redirect(self.get_success_url())
-        return render(request, self.registration_form_template, {"form": form})
+    def invite_by_email(self, email, sender=None, request=None, **kwargs):
+      try:
+          user = self.user_model.objects.get(email=email)
+      except self.user_model.DoesNotExist:
+          user = self.user_model.objects.create(email=email,
+                  password=self.user_model.objects.make_random_password())
+          user.is_active = False
+          user.save()
+      self.send_invitation(user, sender, **kwargs)
+      return user
