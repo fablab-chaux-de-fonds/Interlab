451925c682ca52108c2346ad3e47af426d724eb5
Raphaël Monnard <raphael@monnard.email>
2021-12-05 21:26:56 +0100

    FIX set username as email

--- accounts/backends.py
+++ accounts/backends.py
@@ -10,6 +10,7 @@ from django.utils.translation import gettext as _
 from .models import Profile
 from organizations.backends.defaults import InvitationBackend
 
+import inspect
 
 class CustomInvitations(InvitationBackend):
     """
@@ -20,12 +21,29 @@ class CustomInvitations(InvitationBackend):
         return reverse("profile")
 
     def invite_by_email(self, email, sender=None, request=None, **kwargs):
-      try:
-          user = self.user_model.objects.get(email=email)
-      except self.user_model.DoesNotExist:
-          user = self.user_model.objects.create(email=email,
-                  password=self.user_model.objects.make_random_password())
-          user.is_active = False
-          user.save()
-      self.send_invitation(user, sender, **kwargs)
-      return user
+        """Creates an inactive user with the information we know and then sends
+        an invitation email for that user to complete registration.
+        If your project uses email in a different way then you should make to
+        extend this method as it only checks the `email` attribute for Users.
+        """
+        try:
+            user = self.user_model.objects.get(email=email)
+        except self.user_model.DoesNotExist:
+            # TODO break out user creation process
+            if (
+                "username"
+                in inspect.getfullargspec(self.user_model.objects.create_user).args
+            ):
+                user = self.user_model.objects.create(
+                    username=email,
+                    email=email,
+                    password=self.user_model.objects.make_random_password(),
+                )
+            else:
+                user = self.user_model.objects.create(
+                    email=email, password=self.user_model.objects.make_random_password()
+                )
+            user.is_active = False
+            user.save()
+        self.send_invitation(user, sender, **kwargs)
+        return user
