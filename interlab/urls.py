42757032ad7fbe75edf658a6573d940b999c73ec
Raphaël Monnard <raphael@monnard.email>
2022-01-29 16:15:56 +0100

    init vuejs

--- interlab/urls.py
+++ interlab/urls.py
@@ -25,6 +25,7 @@ urlpatterns += i18n_patterns(
     path('invitations/', include(invitation_backend().get_urls())),
     path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
     path('bootstrap/', views.bootstrap, name='bootstrap'),
+    path('vuejs/', views.vuejs, name='vuejs'),
     path("", include("newsletter.urls")),
     path("", include("cms.urls")),
     prefix_default_language=False
