d86716e147507edf6456ab61f40fd90ea86e6a70
Elsa Weber <elsa.weber@epfl.ch>
2022-02-27 11:41:41 +0100

    Replace add/edit functionality by Django CMS admin feature

---
+++ machines/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+@admin.register(Training)
+class TrainingAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'price', 'machine_category', 'level', 'duration', 'photo']
+
+@admin.register(MachineCategory)
+class MachineCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+@admin.register(OutcomeListItem)
+class TrainingOutcomeAdmin(admin.ModelAdmin):
+    list_display = ['training', 'description']
+
+@admin.register(DIYListItem)
+class TrainingDIYAdmin(admin.ModelAdmin):
+    list_display = ['training', 'title', 'name', 'url']
+
+@admin.register(Faq)
+class TrainingFaqAdmin(admin.ModelAdmin):
+    list_display = ['about', 'question', 'answer']
+
+@admin.register(Machine)
+class MachineAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'price', 'category', 'photo']
--- machines/views.py
+++ machines/views.py
@@ -5,39 +5,12 @@ from django.contrib.admin.views.decorators import staff_member_required
 
 
 from .models import Faq, Training
-from .forms import TrainingForm
 
 
 def trainings_list(request):
     trainings = Training.objects.all()
 
     return render(request, 'trainings/list.html', {'trainings': trainings})
-
-
-@staff_member_required
-def training_edit(request, pk=None):
-    try:
-        obj = Training.objects.get(pk=pk)
-    except Training.DoesNotExist:  # Creation
-        obj = Training()
-
-    if request.method == 'POST':
-        # Update
-        form = TrainingForm(request.POST, request.FILES, instance=obj)
-
-        if form.is_valid():
-            obj = form.save()
-
-            msg = _('Training updated!') if pk else _('Training created!')
-            messages.success(request, msg)
-
-            return redirect('machines:trainings.show', obj.pk)
-
-    else:
-        form = TrainingForm(instance=obj)
-
-    return render(request, 'trainings/edit.html', {'form': form})
-
 
 def training_show(request, pk):
     obj = get_object_or_404(Training, pk=pk)
