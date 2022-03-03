867ef5f133fc06223a207ed1ada4fdbd442869be
Raphaël Monnard <raphael@monnard.email>
2022-03-03 22:28:12 +0100

    Settings back for secret key

--- interlab/settings.py
+++ interlab/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from django.core.management.utils import get_random_secret_key
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ENVIRON_DEBUG = os.environ.get('DEBUG')
