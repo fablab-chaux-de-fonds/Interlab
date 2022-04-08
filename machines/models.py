ac7288b84b8f9349a1a34f76187740b1c4db4c44
Samuel Nussbaum <samuel.nussbaum@erobot.ch>
2022-04-08 22:49:45 +0200

    Styles working in CMS CKEditor, possibility to add video (accept iframes), work on trainings view (machines)

--- machines/models.py
+++ machines/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils.translation import ugettext as _
-
+from djangocms_text_ckeditor.fields import HTMLField
 
 class ItemForRent(models.Model):
     title = models.CharField(max_length=255, verbose_name=_('Title'))
-    description = models.TextField(verbose_name=_('Description'))
-    price = models.FloatField(verbose_name=_('Price'))
+    description = HTMLField(verbose_name=_('Description'),blank=True,configuration='CKEDITOR_SETTINGS')
+    price = models.DecimalField(verbose_name=_('Price'),max_digits=6,decimal_places=2)
 
     def __str__(self):
         return self.title
@@ -13,7 +13,7 @@ class ItemForRent(models.Model):
 
 class MachineCategory(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(blank=True)
+    description = HTMLField(verbose_name=_('Description'),blank=True,configuration='CKEDITOR_SETTINGS')
 
     def __str__(self):
         return self.name
@@ -33,9 +33,9 @@ class Training(ItemForRent):
     machine_category = models.ForeignKey(MachineCategory, on_delete=models.CASCADE, verbose_name=_('Machine category'))
     level = models.CharField(max_length=3, choices=LEVEL_CHOICES, verbose_name=_('Level'))
     duration = models.DurationField(verbose_name=_('Duration'), help_text=_('Use the format HH:MM:SS')) # TODO essayer de trouver un truc plus pratique pour dire la durée
-    # header = ??? # like html header?
-    # support = ??? # isn't it linked to machine instead?
-    # sort = ??? # is it to enforce an order?
+    header = HTMLField(verbose_name=_('Header'),blank=True,configuration='CKEDITOR_SETTINGS')
+    support = HTMLField(verbose_name=_('Support'),blank=True,configuration='CKEDITOR_SETTINGS')
+    sort = models.PositiveSmallIntegerField(default=1)
     photo = models.ImageField(upload_to='trainings', verbose_name=_('Photo'))
 
     def __str__(self):
@@ -49,10 +49,15 @@ class Training(ItemForRent):
     def outcome_list(self):
         return self.outcomelistitem_set.all()
 
+    @property 
+    def machines_list(self):
+        """Query set for Machine with same category"""
+        return Machine.objects.filter(category=self.machine_category)
+
 
 class OutcomeListItem(models.Model):
     training = models.ForeignKey(Training, on_delete=models.CASCADE)
-    description = models.TextField(verbose_name=_('Description'))
+    description = HTMLField(verbose_name=_('Description'),blank=True,configuration='CKEDITOR_SETTINGS')
 
 
 class DIYListItem(models.Model):
@@ -65,20 +70,25 @@ class DIYListItem(models.Model):
 class Faq(models.Model):
     about = models.ForeignKey(ItemForRent, on_delete=models.CASCADE)
     question = models.CharField(max_length=255, verbose_name=_('Question'))
-    answer = models.TextField(verbose_name=_('Answer'))
+    answer = HTMLField(verbose_name=_('Answer'),configuration='CKEDITOR_SETTINGS')
 
+class MachineGroup(models.Model):
+    """For splitting machine in all machines view"""
+    title = models.CharField(max_length=255)
+    description = HTMLField(verbose_name=_('Specifications'),blank=True,configuration='CKEDITOR_SETTINGS')
+    sort = models.PositiveSmallIntegerField(default=1)
 
 class Machine(ItemForRent):
     category = models.ForeignKey(MachineCategory, on_delete=models.CASCADE)  # TODO why exactly ??
     # status = ???
     # model = ??
     visible = models.BooleanField(default=True)
-    reservable = models.BooleanField(default=True)  # isn't it linked to the status??
-    # group = ???
+    reservable = models.BooleanField(default=True)
+    group = models.ForeignKey(MachineGroup,null=True,on_delete=models.SET_NULL) 
     support = models.URLField()
-    spec = models.TextField()  # TODO I think it could be more guided with categories etc
-    # sort = ???
-    subscriber_price = models.FloatField()  # TODO is it for trainings too?
+    spec = HTMLField(verbose_name=_('Specifications'),blank=True,configuration='CKEDITOR_SETTINGS')
+    sort = models.PositiveSmallIntegerField(default=1)
+    subscriber_price = models.DecimalField(verbose_name=_('Subscriber price'),max_digits=6,decimal_places=2)
     photo = models.ImageField(upload_to='machines')
 
 
@@ -87,4 +97,6 @@ class MachineTodoPoint(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     seen_in_training = models.BooleanField(default=False)
-    sort = models.PositiveSmallIntegerField()
+    sort = models.PositiveSmallIntegerField(default=1)
+
+
