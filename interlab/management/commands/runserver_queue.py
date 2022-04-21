a10668f0b446895faeda5c418755084d851d622e
Samuel Nussbaum <samuel.nussbaum@erobot.ch>
2022-04-21 19:42:48 +0200

    missing queue example

---
+++ interlab/management/commands/runserver_queue.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand
+from subprocess import Popen
+from sys import stdout, stdin, stderr
+import time
+import os
+import signal
+
+
+class Command(BaseCommand):
+    help = 'Run all commands'
+    commands = [
+        'python manage.py qcluster',
+        'python manage.py runserver 0.0.0.0:8000'
+    ]
+
+    def handle(self, *args, **options):
+        proc_list = []
+
+        for command in self.commands:
+            print("$ " + command)
+            proc = Popen(command, shell=True, stdin=stdin,
+                         stdout=stdout, stderr=stderr)
+            proc_list.append(proc)
+
+        try:
+            while True:
+                time.sleep(10)
+        except KeyboardInterrupt:
+            for proc in proc_list:
+                os.kill(proc.pid, signal.SIGKILL)
