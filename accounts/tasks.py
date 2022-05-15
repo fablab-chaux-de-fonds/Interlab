c473d250d02e6620541f8ee692a9ea7b602bae54
Raphaël Monnard <raphael.monnard@csem.ch>
2022-05-15 18:36:02 +0200

    NEW task for subscription reminder at 7 days

---
+++ accounts/tasks.py
@@ -0,0 +1,25 @@
+import os
+from django.contrib.sites.models import Site
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext as _
+
+from .models import Profile
+from datetime import date, timedelta
+
+def send_reminder_subscription_email():
+    profiles = Profile.objects.filter(subscription__end = date.today() + timedelta(days = 7))
+    for profile in profiles:
+        context = {
+            'profile': profile,
+            'DOMAIN': Site.objects.first().domain
+        }
+        html_message = render_to_string('accounts/email/subscription_reminder.html', context)
+        send_mail(
+            subject=_('Your subscription exired in 7 days'),
+            from_email=None,
+            message = _("Your subscription exired in 7 days"),
+            recipient_list = [profile.user.email],
+            html_message=html_message
+        )
+    return(list(profiles))
