020ff7bce8ef12647c117597bb2503a30ca30c3b
Samuel Nussbaum <samuel.nussbaum@erobot.ch>
2022-12-25 15:59:27 +0100

    change output to html for coverage

--- test/coverage.sh
+++ test/coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 coverage run --data-file=test/.coverage --source='/code' manage.py test
-coverage xml --data-file=test/.coverage -o test/coverage.xml
+coverage html --data-file=test/.coverage --directory test/html
