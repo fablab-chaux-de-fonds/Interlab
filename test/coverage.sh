7d7fff5d27f44218318eb017b6c6ee8937ca9fd5
Samuel Nussbaum <samuel.nussbaum@erobot.ch>
2022-12-27 22:05:27 +0100

    add coverage badge

--- test/coverage.sh
+++ test/coverage.sh
@@ -2,3 +2,5 @@
 
 coverage run --data-file=test/.coverage --source='/code' manage.py test
 coverage html --data-file=test/.coverage --directory test/html
+cd test
+coverage-badge -o html/coverage.svg
