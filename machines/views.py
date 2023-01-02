0818ae47579ffbf2e152be9f10e7c3b1c52a340c
raphaelmonnard <raphaelmonnard@users.noreply.github.com>
2023-01-02 13:01:19 +0100

    Change class name (#267)
    Co-authored-by: Raphaël Monnard <raphael.monnard@csem.ch>

--- machines/views.py
+++ machines/views.py
@@ -48,7 +48,7 @@ def training_show(request, pk):
     return render(request, 'trainings/show.html', context)
 
 
-class TrainingValidation(LoginRequiredMixin, FormView):
+class TrainingValidationView(LoginRequiredMixin, FormView):
     template_name = 'trainings/training_validation.html'
     form_class = TrainingValidationForm
     success_url = "/" # TODO redirect to training show view
