1ee70592ed63a490daf830ec88bb5f55ff6cc317
Raphaël Monnard <raphael@monnard.email>
2023-01-05 20:32:56 +0100

    Fix #249

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -6,6 +6,10 @@ import * as bootstrap from 'bootstrap';
 import "bootstrap-show-password/dist/bootstrap-show-password.js";
 import "htmx.org";
 
+// Bootstrap - initialize tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
 function importAll(r) {
     return r.keys().map(r);
 }
