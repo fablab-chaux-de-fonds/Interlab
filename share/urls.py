b6900c7c9bbaa21d50d4308575f00226e5c625ed
Raphaël Monnard <raphael@monnard.email>
2023-01-21 15:06:24 +0100

    clean url, remove update

--- share/urls.py
+++ share/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('create', views.PostCreateView.as_view(), name='post_create'),
-    path('update/<int:pk>', views.PostUpdateView.as_view(), name='post_edit'),
     path('delete/<int:pk>', views.PostDeleteView.as_view(), name='post_delete'),
 ]
