f6bdb66163368e127f2ccab327e1a38d40c6d1fe
raphaelmonnard <raphaelmonnard@users.noreply.github.com>
2024-03-01 14:35:39 +0100

    add API app (#543)

---
+++ api/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from accounts.models import CustomUser, Subscription
+from fabcal.models import OpeningSlot, MachineSlot
+from openings.models import Opening
+
+class OpeningSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Opening
+        fields = ['id', 'title']
+
+class OpeningSlotSerializer(serializers.ModelSerializer):
+    duration = serializers.SerializerMethodField()
+
+    class Meta: 
+        model = OpeningSlot
+        fields = ['id', 'start', 'end', 'created_at', 'user', 'opening_id', 'duration']
+
+    def get_duration(self, obj):
+        return obj.get_duration
+
+class MachineSlotSerializer(serializers.ModelSerializer):
+    duration = serializers.SerializerMethodField()
+
+    class Meta: 
+        model = MachineSlot
+        fields = ['id', 'start', 'end', 'created_at', 'updated_at', 'user', 'duration']
+
+    def get_duration(self, obj):
+        return obj.get_duration
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = CustomUser
+        fields = ['id', 'last_login', 'first_name', 'last_name', 'date_joined']
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Subscription
+        fields = ['id', 'start', 'end']
---
+++ api/views.py
@@ -0,0 +1,45 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import generics
+
+from rest_framework.authentication import TokenAuthentication
+from .permissions import IsInApiGroup
+
+from accounts.models import CustomUser, Subscription
+from fabcal.models import OpeningSlot, MachineSlot
+from openings.models import Opening
+
+from .serializers import OpeningSlotSerializer, MachineSlotSerializer
+from .serializers import CustomUserSerializer
+from .serializers import OpeningSerializer
+from .serializers import SubscriptionSerializer
+
+class OpeningSet(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsInApiGroup]
+    queryset = Opening.objects.all()
+    serializer_class = OpeningSerializer
+
+class OpeningSlotSet(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsInApiGroup]
+    queryset = OpeningSlot.objects.all()
+    serializer_class = OpeningSlotSerializer
+
+class MachineSlotSet(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsInApiGroup]
+    queryset = MachineSlot.objects.all()
+    serializer_class = MachineSlotSerializer
+
+class CustomUserSet(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsInApiGroup]
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+class SubscriptionSet(generics.ListAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsInApiGroup]
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
