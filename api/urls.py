19d835c7888e5f819ea3ec8990f1586fddf25080
raphaelmonnard <raphaelmonnard@users.noreply.github.com>
2024-03-01 14:35:39 +0100

    add API app (#543)

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+from . import views
+
+urlpatterns = [
+    re_path('(?P<version>(v1))/opening/', views.OpeningSet.as_view()),
+    re_path('(?P<version>(v1))/opening_slot/', views.OpeningSlotSet.as_view()),
+    re_path('(?P<version>(v1))/machine_slot/', views.MachineSlotSet.as_view()),
+    re_path('(?P<version>(v1))/custom_user/', views.CustomUserSet.as_view()),
+    re_path('(?P<version>(v1))/subscription/', views.SubscriptionSet.as_view()),
+]
