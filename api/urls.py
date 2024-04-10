836db1ee2190db75769ebed56af8f1145ddcbefc
Raphaël Monnard <raphael@monnard.email>
2024-04-10 10:39:11 +0200

    add opening slot in API

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     re_path('(?P<version>(v1))/opening/', views.OpeningSet.as_view()),
     re_path('(?P<version>(v1))/opening_slot/', views.OpeningSlotSet.as_view()),
     re_path('(?P<version>(v1))/machine_slot/', views.MachineSlotSet.as_view()),
+    re_path('(?P<version>(v1))/training_slot/', views.TrainingSlotSet.as_view()),
     re_path('(?P<version>(v1))/custom_user/', views.CustomUserSet.as_view()),
     re_path('(?P<version>(v1))/subscription/', views.SubscriptionSet.as_view()),
     re_path('(?P<version>(v1))/profile/', views.ProfileSet.as_view()),
