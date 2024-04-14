8f32c4c7e5e13c67d6cf9a3089fdef1ca08d3ce2
raphaelmonnard <raphaelmonnard@users.noreply.github.com>
2024-04-14 21:41:36 +0200

    fix issue #553 (#554)
    * fix issue #553
    
    * optimize number of queries

--- fabcal/cms_plugins.py
+++ fabcal/cms_plugins.py
@@ -7,10 +7,14 @@ from babel.dates import format_datetime
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.conf import settings
+from django.contrib.postgres.aggregates import ArrayAgg
+from django.db.models import Value, CharField, F, Subquery, OuterRef
+from django.db.models import Q
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext as _
 
-from .models import WeeklyPluginModel, OpeningSlot, EventSlot, TrainingSlot, CalendarOpeningsPluginModel, EventsListPluginModel, Opening
+from .models import WeeklyPluginModel, OpeningSlot, EventSlot, TrainingSlot, CalendarOpeningsPluginModel, EventsListPluginModel, Opening, MachineSlot
+from machines.models import Machine
 
 from datetime import date, timedelta
 
@@ -49,60 +53,109 @@ class CalendarOpeningsPluginPublisher(CMSPluginBase):
         backend = {}
         backend['events'] = []
 
-        events = list(OpeningSlot.objects.filter(start__gt = date.today() - timedelta(days=365) ))
-        for event in events:
-            backend['events'].append({
-                'type': 'opening',
-                'pk': event.pk,
-                'username': event.user.username,
-                'user_firstname': event.user.first_name,
-                'start': event.start,
-                'end': event.end,
-                'comment': event.comment,
-                'title': event.opening.title,
-                'desc': event.opening.desc,
-                'background_color': event.opening.background_color,
-                'color': event.opening.color,
-                'machines': [{'pk': i.pk, 'title': i.title, 'category': i.category} for i in event.get_machine_list]
-            })
+        opening_slots = OpeningSlot.objects.filter(pk=OuterRef('pk'))
+        machine_slots_subquery = MachineSlot.objects.filter(opening_slot=Subquery(opening_slots)).values_list('machine', flat=True)
 
-        events = list(EventSlot.objects.filter(start__gt = date.today() - timedelta(days=365) ))
-        for event in events:
-            backend['events'].append({
-                'type': 'event',
-                'pk': event.pk,
-                'username': event.user.username,
-                'user_firstname': event.user.first_name,
-                'start': event.start,
-                'end': event.end,
-                'comment': event.comment,
-                'title': event.event.title,
-                'desc': event.event.lead,
-                'background_color': event.event.background_color,
-                'color': event.event.color
-            })
+        # Combine the three queries into one
+        events = list(
+            OpeningSlot.objects.filter(start__gt=datetime.datetime.now() - timedelta(days=365))
+            .annotate(
+                type=Value('opening', output_field=CharField()),
+                user_firstname=F('user__first_name'),
+                username=F('user__username'),
+                title=F('opening__title'),
+                desc=F('opening__desc'),
+                background_color=F('opening__background_color'),
+                color=F('opening__color'),
+                machines=ArrayAgg('machineslot__machine')
+            )
+            .values(
+                'type',
+                'pk',
+                'username',
+                'user_firstname',
+                'start',
+                'end',
+                'comment',
+                'title',
+                'desc',
+                'background_color',
+                'color',
+                'machines'
+            )
+        ) + list(
+            EventSlot.objects.filter(start__gt=datetime.datetime.now() - timedelta(days=365))
+            .annotate(
+                type=Value('event', output_field=CharField()),
+                user_firstname=F('user__first_name'),
+                username=F('user__username'),
+                title=F('event__title'),
+                desc=F('event__lead'),
+                background_color=F('event__background_color'),
+                color=F('event__color')
+            )
+            .values(
+                'type',
+                'pk',
+                'username',
+                'user_firstname',
+                'start',
+                'end',
+                'comment',
+                'title',
+                'desc',
+                'background_color',
+                'color'
+            )
+        ) + list(
+            TrainingSlot.objects.filter(start__gt=datetime.datetime.now() - timedelta(days=365))
+            .annotate(
+                type=Value('training', output_field=CharField()),
+                user_firstname=F('user__first_name'),
+                username=F('user__username'),
+                title=F('training__title'),
+                background_color=Value('#ddf9ff', output_field=CharField()),
+                color=Value('#0b1783', output_field=CharField())
+            )
+            .values(
+                'type',
+                'training__pk',
+                'username',
+                'user_firstname',
+                'start',
+                'end',
+                'comment',
+                'title',
+                'background_color',
+                'color'
+            )
+        )
 
-        events = list(TrainingSlot.objects.filter(start__gt = date.today() - timedelta(days=365) ))
-        for event in events:
-            backend['events'].append({
-                'type': 'training',
-                'pk': event.training.pk,
-                'username': event.user.username,
-                'user_firstname': event.user.first_name,
-                'start': event.start,
-                'end': event.end,
-                'comment': event.comment,
-                'title': event.training.title,
-                'background_color': '#ddf9ff',
-                'color': '#0b1783',
-                })
-        
-        backend["is_superuser"] = request.user.groups.filter(name='superuser').exists()
-        backend["username"] = request.user.username
 
-        context = {
-            'backend': json.dumps(backend, default=str),
-            'public_openings': Opening.objects.filter(is_public=True)
+        # Fetch all Machine objects corresponding to machine IDs in events
+        machine_ids = set(machine_id for event in events if 'machines' in event for machine_id in event['machines'])
+        machines = Machine.objects.filter(pk__in=machine_ids)
+
+        # Create a dictionary to map machine IDs to Machine objects
+        machine_mapping = {machine.pk: machine for machine in machines}
+
+        # Iterate over the events and update the 'machines' field
+        for event in events:
+            if 'machines' in event:
+                # Map machine IDs to Machine objects and convert to a list of dictionaries
+                machine_list = [{'pk': machine_id, 'title': machine_mapping[machine_id].title, 'category': machine_mapping[machine_id].category} for machine_id in event['machines'] if machine_id]
+                # Replace the machine IDs with the list of dictionaries
+                event['machines'] = machine_list
+
+            # Append the modified event to the backend dictionary
+            backend['events'].append(event)
+
+            backend["is_superuser"] = request.user.groups.filter(name='superuser').exists()
+            backend["username"] = request.user.username
+
+            context = {
+                'backend': json.dumps(backend, default=str),
+                'public_openings': Opening.objects.filter(is_public=True)
             }
         return context
 
